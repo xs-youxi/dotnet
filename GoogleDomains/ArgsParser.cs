f38c83471e3f8fab976ed84a521b80b3e1096a13
Seonghee Won <xsyouxi@gmail.com>
2016-12-04 02:15:17 +0900

    add google ddns

---
+++ GoogleDomains/ArgsParser.cs
@@ -0,0 +1,29 @@
+﻿namespace GoogleDomains
+{
+    public class ArgsParser
+    {
+#if false
+        public static void NDeskParser(string[] args)
+        {
+            string username = null, password = null, hostname = null, myip = null, offline = null;
+
+            var p = new OptionSet()
+                          .Add("host|hostname=", $"hostname is {hostname}", (v) => hostname = v)
+                          .Add("u|username=", $"username is {username}", (v) => username = v)
+                          .Add("p|password=", $"username is {password}", (v) => password = v)
+                          .Add("ip|myip=", $"myip is {myip}", (v) => myip = v)
+                          .Add("o|offline=", $"myip is {offline}", (v) => offline = v)
+                          .Add("h|?|help", (v) => { Console.WriteLine("help"); });
+            try
+            {
+                var extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+#endif
+    }
+}
---
+++ GoogleDomains/Program.cs
@@ -0,0 +1,77 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleDomains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arguments = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            if (arguments.Errors.Any())
+                return;
+            var argsBucket = arguments.Value;
+            try
+            {
+                var task = UpdateGoogleDomainsAsync(argsBucket.hostname, argsBucket.username, argsBucket.password, argsBucket.myip, argsBucket.offline);
+                task.ContinueWith(r =>
+                   {
+                       Console.WriteLine("");
+                       Console.WriteLine(r.Result);
+                   });
+
+                var autoEvent = new AutoResetEvent(false);
+                var timer = new Timer((c) =>
+                {
+                    Console.Write($">");
+                    if (task.IsCompleted)
+                        autoEvent.Set();
+                }, null, 0, 100);
+                autoEvent.WaitOne();
+                timer.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        class Options
+        {
+            [Value(0, Required = true)]
+            public string hostname { get; set; }
+            [Option('u', "username", Required = true, HelpText = "Input username.")]
+            public string username { get; set; }
+            [Option('p', "password", Required = true, HelpText = "Input password.")]
+            public string password { get; set; }
+            [Option('i', "myip", HelpText = "Input myip.")]
+            public string myip { get; set; }
+            [Option('o', "offline", HelpText = "yes or no")]
+            public string offline { get; set; }
+        }
+        async public static Task<string> UpdateGoogleDomainsAsync(string hostname, string username, string password, string myip = null, string offline = null)
+        {
+            // ref https://support.google.com/domains/answer/6147083
+            var uri = new Uri($"https://domains.google.com/nic/update");
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(username, password);
+
+                var reqParams = new NameValueCollection();
+                reqParams.Add("hostname", hostname);
+                if (myip != null)
+                    reqParams.Add("myip", myip);
+                if (offline != null)
+                    reqParams.Add("offline", offline);
+
+                var responseArray = await client.UploadValuesTaskAsync(uri, "POST", reqParams);
+                return Encoding.UTF8.GetString(responseArray);
+            }
+        }
+    }
+}
